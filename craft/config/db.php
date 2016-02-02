083829666970e68f5c2e700ebfbfe44d07c78394
billCaplan <bill.j.caplan@gmail.com>
2016-02-02 13:54:24 -0800

    New First Commit with permissions

---
+++ craft/config/db.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+
+return array(
+
+	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+	'server' => $url[“host”],
+
+
+	// The database username to connect with.
+	'user' => $url[“user”],
+
+	// The database password to connect with.
+	'password' => $url[“pass”],
+
+	// The name of the database to select.
+	'database' => substr($url[“path”],1),
+
+	// The prefix to use when naming tables. This can be no more than 5 characters.
+	'tablePrefix' => 'craft',
+
+);
