65ac82ad131372e5ea8d38f95f4af4f0ebc5288e
billCaplan <bill.j.caplan@gmail.com>
2016-02-02 17:17:04 -0800

    Added db info

--- craft/config/db.php
+++ craft/config/db.php
@@ -14,13 +14,13 @@ return array(
 
 
 	// The database username to connect with.
-	'user' => $url[“user”],
+	'user' => 'bb4facee42109f',
 
 	// The database password to connect with.
-	'password' => $url[“pass”],
+	'password' => '217f7d67',
 
 	// The name of the database to select.
-	'database' => substr($url[“path”],1),
+	'database' => 'heroku_85aea75a5cc5df7',
 
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
